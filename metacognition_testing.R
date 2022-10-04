ea0d61787adf5c282427c8ae48bc3016cf1eeb98
Kelly Hoogervorst <kellyhoogervorst19@gmail.com>
2022-10-04 16:02:13 +0200

    more testing > compare metacog data

---
+++ metacognition_testing.R
@@ -0,0 +1,7 @@
+# testing all metacognition ratings
+
+# converting the accuracy data
+
+
+#loading in the pre and post confidence scores
+meta <- all_data[, 1:12]
--- test by accuracy.R
+++ test by accuracy.R
@@ -24,6 +24,7 @@ acctab <- filter(acctab, subj!=996)
 acctab <- na.omit(acctab)
 
 # plot
+library(ggplot2)
 acctab[, 4] <- as.character(unlist(acctab[, 4])) # because numbers were integers
 
 acctab %>%
@@ -37,4 +38,51 @@ acctab %>%
 # mixed factorial anova
 library(lme4)
 library(Matrix)
+library(tidyr)
+library(afex)
+library(performance)
+library(emmeans)
 
+# data is already in long format, nice
+acctab <- acctab[acctab$subj != 173,]
+
+mixed_anova <- aov_ez(
+  id = "subj",
+  dv = "conf",
+  data = acctab,
+  between = "gender",
+  within = c("mod", "acc")
+)
+
+test_sphericity(mixed_anova)
+
+afex_plot(mixed_anova, x = "mod", trace = "gender")
+
+# alternative code
+library(rstatix)
+colnames(acctab) <- cbind("subj", "gender", "mod", "acc", "conf")
+res.aov <- acctab %>%
+            ungroup() %>%
+             anova_test(dv = "conf", wid = "subj", between = "gender", within = c("mod", "acc"))
+
+get_anova_table(res.aov)
+
+#post hoc
+pwc <- acctab %>%
+  ungroup() %>%
+  pairwise_t_test(
+    conf ~ c(gender, mod, acc), paired = TRUE, 
+    p.adjust.method = "bonferroni"
+  ) %>%
+  select(-statistic, -df) # Remove details
+
+# accuracy difference -----------------------------------------------------
+
+deltacc <- acctab %>%
+  group_by(subj, gender, mod) %>%
+  summarise(conf[acc==1]-conf[acc==0])
+colnames(deltacc) <- cbind("subj", "gender", "mod", "delta_conf")
+
+deltacc %>%
+  ggplot(aes(x = mod, y = delta_conf, fill = gender))+
+  geom_boxplot(notch = T)
